72e8464929827595b94df8fc4a708bc28e1965a8
Tramia McGee <57148089+miamichellee@users.noreply.github.com>
2019-12-04 15:44:11 -0500

    Update Playlist.h

--- Playlist.h
+++ Playlist.h
@@ -1,1 +1,41 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+#include "Song.h"
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+class Playlist
+{
+   private:
+    std::vector<Song> lsong;
+    int currentsong;
+    std::string p_name;
+    static char option;
+    
+   public:
+    Playlist();
+    Playlist(std::string); 
+    
+    void addSong(Song& );
+    void deleteSong(Song& );
+    Playlist intersect(Playlist& );
+    bool search(std::vector<Song>, Song& );
+    friend Playlist operator+(Playlist& , Playlist& );
+    friend Playlist operator+(Playlist& , Song& );
+    friend Playlist operator-(Playlist& , Song& );
+    friend std::ostream& operator<<(std::ostream& , const Playlist& );
+    void play();
+    static void mode(char );
+    std::vector<Song> getSong();
+    void setPName(std::string );
+    std::string getPName();
+    
+    
+    
+};
+#endif
